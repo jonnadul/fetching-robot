b2910f6c1224d2e72d208e1f3ff582dd3ee80c43
root <root@arm.(none)>
2012-11-30 21:39:09 -0600

    UDP code in python

--- bbcode/udp.py
+++ bbcode/udp.py
@@ -1,11 +1,26 @@
+from struct import *
 import socket
-
-UDP_IP = "127.0.0.1"
 UDP_PORT = 54259
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP,UDP_PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
+try:
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+except AttributeError:
+     pass
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32) 
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
+
+sock.bind(('', 54259))
+host = socket.gethostbyname(socket.gethostname())
+sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(host))
+sock.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP, 
+                   socket.inet_aton('224.1.1.1') + socket.inet_aton(host))
+
+
+#sock.bind(('localhost',UDP_PORT))
 while True:
 	data, addr = sock.recvfrom(1024)
-	print "received message:", data
+	#print "received message:", data
+	ulx,uly,brx,bry = unpack('!llll',data[0:16])
+	print str(ulx), str(uly), str(brx), str(bry)
